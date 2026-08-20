f7df2383a5dfca8eb8b1889705dfd8d8eb775fa3
bbottstql <bbotts@tql.com>
2026-08-20 09:39:08 -0400

    add requested by filter and fixed api to facilitate

--- Prs.Api/Controllers/RequestsController.cs
+++ Prs.Api/Controllers/RequestsController.cs
@@ -18,15 +18,19 @@ namespace Prs.Api.Controllers {
         // GET: api/Requests?status=NEW
         // GET: api/Requests?status=REVIEW
         // GET: api/Requests?status=APPROVED
-        // GET: api/Requests?status=REJECTED
+        // GET: api/Requests?status=REJECTED&userId=5
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Request>>> GetAll([FromQuery] string? status = null) {
+        public async Task<ActionResult<IEnumerable<Request>>> GetAll([FromQuery] string? status = null, [FromQuery] int? userId = null) {
             var query = _db.Requests
                            .Include(request => request.User)
                            .AsQueryable();
 
             if (status != null) {
-                query = query.Where(request => request.Status != status);
+                query = query.Where(request => request.Status == status);
+            }
+
+            if (userId != null) {
+                query = query.Where(request => request.UserId == userId);
             }
 
             return await query.ToListAsync();
